9551a558e90dbe0201748819afd6265b2f91f2a5
Vidhate Rohit <rohitvidhate77@gmail.com>
2024-01-13 15:47:57 +0530

    added auth feature

---
+++ src/Data/RegisteredUsers.js
@@ -0,0 +1,6 @@
+export const REGISTERED_USERA_LIST = [
+  { email: "test123@gmail.com", password: "test123" },
+  { email: "test111@gmail.com", password: "test111" },
+  { email: "test222@gmail.com", password: "test222" },
+  { email: "test333@gmail.com", password: "test333" },
+];
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,75 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoggedIn } from "../Slices/RegisteredUsersSlice";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const registeredUsers = useSelector(
+    (store) => store.registeredUsers.registeredUsers
+  );
+
+  console.log(registeredUsers);
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const matchingUser = registeredUsers.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (matchingUser) {
+      dispatch(setLoggedIn(true));
+      navigate("/bag");
+    } else {
+      console.log("Invalid email or password");
+    }
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <>
       <div className="login-container">
-        <p>
-          <strong>Login</strong> or <strong>Signup</strong>{" "}
+        <p className="heading">
+          <strong className="heading-name">Login</strong> or{" "}
+          <strong className="heading-name">Signup</strong>{" "}
         </p>
         <div className="box">
           <input
             className="input-box"
             type="text"
             placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />{" "}
-          <br />
           <input
             className="input-box"
             type="text"
             placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="btn-place-order btn-signin">SIGN IN</button>
+          <div className="bottom-box">
+            <p>
+              I agree to the <strong className="strng">Term of Use</strong> &{" "}
+              <strong className="strng">Privacy Policy</strong>
+            </p>
+            <button
+              className="btn-place-order btn-signin"
+              onClick={handleSubmit}
+            >
+              SIGN IN
+            </button>
+            <p>
+              Have trouble logging in?{" "}
+              <strong className="strng">Get Help</strong>
+            </p>
+          </div>
         </div>
       </div>
     </>
